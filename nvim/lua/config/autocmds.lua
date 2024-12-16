9405949fab4dbff6b20b95301ec6f0baacc51af0
jstringara <62561822+jstringara@users.noreply.github.com>
2024-12-16 12:39:22 +0100

    Add automatic wrap for txt and md files

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,3 +1,10 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- set wrap on .md and .txt files
+vim.api.nvim_create_autocmd("BufEnter", {
+    pattern = { "*.md", "*.txt" },
+    group = group,
+    command = "setlocal wrap",
+})
